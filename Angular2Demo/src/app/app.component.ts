e9d2680e30e2c70eb5cb69c2fc29bfacd0c7d3c9
Rohit Kumar Rawal <rohit.rawal@accionlabs.com>
2019-07-18 12:51:03 +0530

    First

---
+++ Angular2Demo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+    //template: `<h1>Hello {{name}}</h1>`,
+    //templateUrl: 'app/app.component.html',
+    template: '<div><h1>{{pageHeader}}</h1><my-employee></my-employee></div>'
+})
+//export class AppComponent  { name = 'Angular 2!'; }
+export class AppComponent {
+    pageHeader: string = 'Employee Details';
+}
